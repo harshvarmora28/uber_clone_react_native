bfa37de40a00d254c5584c85b64848e03fb7cb34
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-17 14:02:45 +0530

    working on implementing google cloud platform apis

--- App.js
+++ App.js
@@ -2,19 +2,38 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 // import "react-native-gesture-handler";
-// import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      {/* <NavigationContainer> */}
+      <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+          <Stack.Navigator>
+            <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              headerShown: false
+            }}
+            />
+            <Stack.Screen
+            name="MapScreen"
+            component={MapScreen}
+            options={{
+              headerShown: false
+            }}
+            />
+          </Stack.Navigator>
         </SafeAreaProvider>
-        {/* </NavigationContainer> */}
+      </NavigationContainer>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env"
 
 const HomeScreen = () => {
   return (
@@ -16,6 +18,11 @@ const HomeScreen = () => {
           source={{
             uri: "https://links.papareact.com/gzs",
           }}
+        />
+        <GooglePlacesAutocomplete
+        placeholder="Where From?"
+        nearbyPlacesAPI="GooglePlacesSearch"
+        debounce={400}
         />
         <NavOptions/>
       </View>
