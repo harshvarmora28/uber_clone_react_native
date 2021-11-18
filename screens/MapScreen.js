758298ceca396354068d983eacb433ba47e6ef9a
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-18 13:25:15 +0530

    rendered google maps in the app

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+import Map from '../components/Map'
+import MapView from 'react-native-maps'
 
 const MapScreen = () => {
     return (
         <View>
-            <Text>here the map will load...</Text>
+            <View style={tw`h-1/2`}>
+                <Map/>
+            </View>
+            <View style={tw`h-1/2`}>
+
+            </View>
         </View>
     )
 }
